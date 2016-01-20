98dc63dd5d8d963957bfbb7f6afe567e80c87744
devintyler <dtv9019565DAE>
2016-01-20 16:06:55 -0500

    Syncano JS Library example, not a request

--- payloadTrigger.js
+++ payloadTrigger.js
@@ -1,49 +1,13 @@
 // Dancing Potato Example -
-// http://docs.syncano.io/docs/webhooks#section-step-3-run-a-webhook-with-payload-args-
-var request = require('request');
+// http://docs.syncano.io/docs/webhooks#section-step-3-run-a-webhook-with-payload-args
+var Syncano = require('syncano');
 
-var url = "https://api.syncano.io/v1/instances/blue-pond-1563/webhooks/p/d66e885ce616e232d1230e6b0f1e3c6cc255582e/payloadexample/";
-var payloadName = "user_search";
-var payload = "dancing";
-var payloadURL = url + "?" + payloadName + "=" + payload;
+var account = Syncano({accountKey: 'ACCOUNNTKEY'});
 
-request.get(payloadURL, function(error, response, body){ // GET request
-    if (!error && response.statusCode == 200){
-        var bodyObj = JSON.parse(body);
-        console.log(bodyObj.result.stdout);
-    } else {
-        console.log("Error: " + error);
-    }
-});
-
-request({ // POST request
-    url: url,
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: '{"' + payloadName + '":"' + payload + '"}'
-}, function(error, response, body){
-    if (!error && response.statusCode == 200) {
-        var bodyObj = JSON.parse(body); // convert the string to a JSON
-        console.log(bodyObj.result.stdout);
-    } else {
-        console.log("Error: " + error);
-    }
-});
-
-//request({ // BOTH
-//    url: payloadURL,
-//    method: 'POST',
-//    headers: {
-//        'Content-Type': 'application/json'
-//    },
-//    body: '{"' + payloadName + '":"This is your POST data!"}' //Set the body as a string
-//}, function(error, response, body){
-//    if (!error && response.statusCode == 200) {
-//        var bodyObj = JSON.parse(body); // convert the string to a JSON
-//        console.log(bodyObj.result.stdout);
-//    } else {
-//        console.log("Error: " + error);
-//    }
-//});
+account.instance('INSTANCE').webhook('WEBHOOK').run({"user_search":"dancing"})
+    .then(function(res){
+        console.log(res.result.stdout);
+    })
+    .catch(function(err){
+        console.log(err);
+    });
